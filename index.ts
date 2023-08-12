9e336261d40f5c58bdebfa08e6a3314b2b48f7ec
Alinawaz <139918066+TypescriptAlinawaz@users.noreply.github.com>
2023-08-12 21:19:26 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+/*Number Eight: Write addition, subtraction, multiplication, and c operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.*/
+
+//step 1: AdditionResult.
+
+const additionResult = 4+4;
+
+console.log ("AdditionResult        :  " +    additionResult)
+
+//step 2: SubtractionResult.
+
+const subtractionResult =  16-8;
+
+console.log("SubtractionResult     :  " +   subtractionResult)
+
+//step 3: MultiplicationResult .
+
+const multiplicationResult =  4*2;
+
+console.log("MultiplicationResult  :  " + multiplicationResult)
+
+
+//step 4: DivisionResult .
+
+const divisionResult =  16/2;
+
+console.log("DivisionResult        :  " +   divisionResult)
+
+
